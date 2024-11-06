3f3a5e15b6c3a33cb478534b4acd8b6de4d0e589
zheng921106 <102711416+zheng921106@users.noreply.github.com>
2024-11-07 01:51:55 +0800

    2024-11-04

--- src/components/header.js
+++ src/components/header.js
@@ -4,25 +4,36 @@ import 'element-plus/dist/index.css';
 
 export function renderHeader() {
     return `
-    <div class="global-header">
-      <div class="header-left">
-        <img src="https://www.yasyadong.com/data/upload/common/07242086604829132.png" alt="Logo" class="logo">
-        <el-menu mode="horizontal" background-color="#333" text-color="#fff" active-text-color="#ff6b6b">
-          <el-submenu index="1">
-            <template #title><i class="el-icon-menu"></i> yas 오리지널</template>
-            <el-menu-item index="1-1">yas 오리지널</el-menu-item>
-            <el-menu-item index="1-2">yas 플러스</el-menu-item> 
-          </el-submenu>
-          <el-menu-item index="2">기타 메뉴</el-menu-item> 
-        </el-menu>  
-      </div>
-      <div class="header-center">
-        <el-input placeholder="Search..." suffix-icon="el-icon-search" class="search-bar"></el-input>
-      </div>
-      <div class="header-right">
-        <a href="/login" class="login-link">로그인 / 회원가입</a>
-      </div>
-    </div>
+    <el-container>
+      <el-header class="global-header">
+        <div class="header-left">
+          <img src="https://www.yasyadong.com/data/upload/common/07242086604829132.png" alt="Logo" class="logo">
+          <el-menu mode="horizontal" background-color="#333" text-color="#fff" active-text-color="#ff6b6b">
+            <el-submenu index="1">
+              <template #title><i class="el-icon-menu"></i> yas 오리지널</template>
+              <el-menu-item index="1-1">yas 오리지널</el-menu-item>
+              <el-menu-item index="1-2">yas 플러스</el-menu-item>
+            </el-submenu>
+            <el-menu-item index="2">기타 메뉴</el-menu-item>
+          </el-menu>  
+        </div>
+        <div class="header-center">
+          <el-input placeholder="Search..." suffix-icon="el-icon-search" class="search-bar"></el-input>
+        </div>
+        <div class="header-right">
+          <a href="/login" class="login-link">로그인 / 회원가입</a>
+        </div>
+      </el-header>
+
+      <el-container>
+        <el-aside width="200px" class="aside">Aside</el-aside>
+        <el-container>
+          <el-main class="main-content">Main Content</el-main>
+          <el-footer class="footer">Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
+
     <style>
       /* Header 样式 */
       .global-header {
@@ -63,6 +74,21 @@ export function renderHeader() {
       }
       .login-link:hover {
         text-decoration: underline;
+      }
+      /* Aside, Main, Footer 样式 */
+      .aside {
+        background-color: #333;
+        color: #fff;
+        padding: 15px;
+      }
+      .main-content {
+        padding: 20px;
+      }
+      .footer {
+        background-color: #222;
+        color: #fff;
+        text-align: center;
+        padding: 10px 0;
       }
     </style>
   `;
